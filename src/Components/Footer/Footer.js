2083e5609b878e9576aa03080b05f74c5aecfbbd
shreya kumari <104058567+shreypilot@users.noreply.github.com>
2022-10-17 20:17:23 +0530

    Create Footer.js

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+  return <>
+    <div className="footer">
+        <img src="icon-white.png"/>
+    </div>
+  </>;
+}
+
+export default Footer;
